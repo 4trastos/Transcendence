42bf5b3dc4ea880b933b39c2d568f5068de1627b
4trastos <93524280+4trastos@users.noreply.github.com>
2025-02-07 13:11:11 +0100

    Add files via upload

---
+++ src/backend/tools/init.sql
@@ -0,0 +1,17 @@
+-- init.sql en tools/
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS games (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    player1_id INTEGER,
+    player2_id INTEGER,
+    winner_id INTEGER,
+    FOREIGN KEY (player1_id) REFERENCES users (id),
+    FOREIGN KEY (player2_id) REFERENCES users (id),
+    FOREIGN KEY (winner_id) REFERENCES users (id)
+);
