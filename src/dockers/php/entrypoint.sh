46792701aac6043b8708a41bdbca0725b3144a6d
4trastos <93524280+4trastos@users.noreply.github.com>
2025-03-18 12:54:53 +0100

    Add files via upload

--- src/dockers/php/entrypoint.sh
+++ src/dockers/php/entrypoint.sh
@@ -11,5 +11,9 @@ chown -R www-data:www-data /zap/reports
 # Crear un enlace simbólico desde /var/www/html/zap_reports a /zap/reports
 ln -sf /zap/reports /var/www/html/zap_reports
 
+# Iniciar PHP-FPM y Nginx
+php-fpm &
+nginx -g 'daemon off;'
+
 # Iniciar Apache
-exec apache2-foreground
+#exec apache2-foreground
