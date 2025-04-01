9dad4f547b3f7f20c6929a814f832c3d6647da92
4trastos <93524280+4trastos@users.noreply.github.com>
2025-04-01 14:03:52 +0200

    Delete src directory

--- src/dockers/management/elasticsearch/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Solo asegurar permisos de los datos (elimina la línea del chmod externo)
-chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-
-# Ejecutar el comando original
-exec /usr/local/bin/docker-entrypoint.sh "$@"
