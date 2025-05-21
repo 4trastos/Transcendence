40ae679cd88ad356ddb4835fdf27de0ea7a0c0c7
4trastos <info@4trastos.com>
2025-05-21 21:29:18 +0200

    Prometheus y Grafana OK

--- script/display_art.sh
+++ script/display_art.sh
@@ -69,7 +69,7 @@ ART=(
 "                                                                                                                                                                    " \
 "                                                                                                                                                                    " \
 "                                                           · A project conceived, designed, and executed by: ·                                                      " \
-"                                                      Nicolás González De Mendoza & David Gallego & Adrian Herrera                                                  " \
+"                                               Nicolás González De Mendoza & David Gallego & Adrian Herrera && Iñigo Alvarez                                        " \
 "                                                                                                                                                                    " \
 "                                                                            *  Since 2025  *                                                                        " \
 "                                                                                                                                                                    " \
